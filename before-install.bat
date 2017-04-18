269267c464b60311ca57855a969944bae1c9b25b
fiona.liufang <ziyouhuxi0@163.com>
2017-04-18 13:14:03 +0800

    file

---
+++ before-install.bat
@@ -0,0 +1,3 @@
+﻿REM Install Internet Information Server (IIS).
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
